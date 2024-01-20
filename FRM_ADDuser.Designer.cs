6ea8e0120032f94cd84ccdf6c662a96df3db544d
mustafa <155826815+mustafaAlzmrly@users.noreply.github.com>
2024-01-20 22:45:06 +0200

    واجهة إضافة مستخدم

---
+++ FRM_ADDuser.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace AMS_PRO_MAX
+{
+    partial class FRM_ADDuser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edt_password = new System.Windows.Forms.TextBox();
+            this.edt_username = new System.Windows.Forms.TextBox();
+            this.edt_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(207, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "كلمة السر";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(180, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "اسم المستخدم";
+            // 
+            // edt_password
+            // 
+            this.edt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edt_password.Location = new System.Drawing.Point(22, 253);
+            this.edt_password.Name = "edt_password";
+            this.edt_password.Size = new System.Drawing.Size(474, 34);
+            this.edt_password.TabIndex = 6;
+            this.edt_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // edt_username
+            // 
+            this.edt_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edt_username.Location = new System.Drawing.Point(22, 164);
+            this.edt_username.Name = "edt_username";
+            this.edt_username.Size = new System.Drawing.Size(474, 34);
+            this.edt_username.TabIndex = 7;
+            this.edt_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // edt_name
+            // 
+            this.edt_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edt_name.Location = new System.Drawing.Point(22, 82);
+            this.edt_name.Name = "edt_name";
+            this.edt_name.Size = new System.Drawing.Size(474, 34);
+            this.edt_name.TabIndex = 8;
+            this.edt_name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(193, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "الاسم الكامل";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 397);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(516, 63);
+            this.panel1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.Image = global::AMS_PRO_MAX.Properties.Resources.save_32x32;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(516, 63);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "اضافة";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FRM_ADDuser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 460);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.edt_password);
+            this.Controls.Add(this.edt_username);
+            this.Controls.Add(this.edt_name);
+            this.Controls.Add(this.label1);
+            this.Name = "FRM_ADDuser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ADDuser";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox edt_password;
+        public System.Windows.Forms.TextBox edt_username;
+        public System.Windows.Forms.TextBox edt_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Button button1;
+    }
+}
---
+++ FRM_ADDuser.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AMS_PRO_MAX
+{
+    public partial class FRM_ADDuser : Form
+    {
+
+        DB_AMS_PROEntities5 db = new DB_AMS_PROEntities5();
+        User add;
+        Form1 form1;
+        public int id;
+        bool state;
+        public FRM_ADDuser()
+        {
+            InitializeComponent();
+        }
+        private bool Save()
+        {
+            dialog dialog = new dialog();
+            form1 = new Form1();
+            // check fields
+
+            if (edt_name.Text == "" || edt_password.Text == "" || edt_username.Text == "")
+            {
+                dialog.Width = this.Width;
+                dialog.label6.Text = "اكمل الحقل لطفا";
+                dialog.Show();
+            }
+            else
+            {
+                // Add or edit
+                var checkDuplicate = CheckDuplicateData();
+                if (id == 0)
+                {
+                    // Add
+                    // Check Duplicate Data
+                    
+                    if (checkDuplicate == true)
+                    {
+                        dialog.Width = this.Width;
+                        dialog.label6.Text = "اسم الصنف مكرر";
+                        dialog.Show();
+
+                    }
+                    else
+                    {
+                        // Add
+                        AddData();
+                        state = true;
+                        dialog.Width = this.Width;
+                        dialog.label6.Text = "تمت اضافة مستخدم جديد من قبل "+form1.txt_username.Text;
+                        dialog.Show();
+
+
+
+                    }
+
+                }
+                else
+                {
+                    // Edit
+
+
+
+                }
+            }
+            return state;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Save();
+
+        }
+        private bool CheckDuplicateData()
+        {
+            try
+            {
+                db = new DB_AMS_PROEntities5();
+                add = db.Users.Where(x => x.Username == edt_username.Text).FirstOrDefault();
+                if (add == null)
+                {
+                    state = false;
+                }
+                else
+                {
+                    state = true;
+                }
+            }
+            catch
+            {
+                state = false;
+                
+            }
+
+
+            return state;
+
+        }
+        private void AddData()
+        {
+            try
+            {
+                db = new DB_AMS_PROEntities5();
+                add = new User()
+                {
+
+                    Fullname = edt_name.Text,
+                    Username = edt_username.Text,
+                    Password = edt_password.Text,
+
+                };
+                db.Entry(add).State = System.Data.Entity.EntityState.Added;
+                db.SaveChanges();
+                this.Close();
+                
+
+            }
+            catch
+            {
+               
+
+            }
+        }
+    }
+}
