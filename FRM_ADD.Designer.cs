557bbe428ced0be0a9a752746dcc99a35009670f
mustafa <155826815+mustafaAlzmrly@users.noreply.github.com>
2024-01-20 22:46:23 +0200

    واجهة إضافة صنف

---
+++ FRM_ADD.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace AMS_PRO_MAX
+{
+    partial class FRM_ADD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pic_cover = new System.Windows.Forms.PictureBox();
+            this.txt_qun = new System.Windows.Forms.TextBox();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.txt_des = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_dat = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cover)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_dat);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btn_add);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.pic_cover);
+            this.panel1.Controls.Add(this.txt_qun);
+            this.panel1.Controls.Add(this.txt_price);
+            this.panel1.Controls.Add(this.txt_des);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txt_name);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(718, 660);
+            this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = global::AMS_PRO_MAX.Properties.Resources.cancel_32x32;
+            this.button4.Location = new System.Drawing.Point(9, 9);
+            this.button4.Margin = new System.Windows.Forms.Padding(0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(41, 34);
+            this.button4.TabIndex = 22;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::AMS_PRO_MAX.Properties.Resources.cancel_32x32;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(576, 584);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 64);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "الغاء";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Image = global::AMS_PRO_MAX.Properties.Resources.addfile_32x32;
+            this.btn_add.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_add.Location = new System.Drawing.Point(461, 584);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(99, 64);
+            this.btn_add.TabIndex = 20;
+            this.btn_add.Text = "اضافة";
+            this.btn_add.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(464, 451);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 41);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "صورة الصنف";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(539, 349);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 41);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "صلاحية";
+            // 
+            // pic_cover
+            // 
+            this.pic_cover.Image = global::AMS_PRO_MAX.Properties.Resources.btn_categories_ImageOptions_Image;
+            this.pic_cover.Location = new System.Drawing.Point(157, 413);
+            this.pic_cover.Name = "pic_cover";
+            this.pic_cover.Size = new System.Drawing.Size(175, 168);
+            this.pic_cover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_cover.TabIndex = 16;
+            this.pic_cover.TabStop = false;
+            // 
+            // txt_qun
+            // 
+            this.txt_qun.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_qun.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_qun.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txt_qun.Location = new System.Drawing.Point(157, 133);
+            this.txt_qun.Name = "txt_qun";
+            this.txt_qun.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txt_qun.Size = new System.Drawing.Size(308, 34);
+            this.txt_qun.TabIndex = 15;
+            // 
+            // txt_price
+            // 
+            this.txt_price.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_price.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_price.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txt_price.Location = new System.Drawing.Point(157, 193);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txt_price.Size = new System.Drawing.Size(308, 34);
+            this.txt_price.TabIndex = 14;
+            // 
+            // txt_des
+            // 
+            this.txt_des.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_des.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_des.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txt_des.Location = new System.Drawing.Point(157, 253);
+            this.txt_des.Name = "txt_des";
+            this.txt_des.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txt_des.Size = new System.Drawing.Size(308, 34);
+            this.txt_des.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(537, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 41);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "الوصف";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(556, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 41);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "السعر";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(554, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 41);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "الكمية";
+            // 
+            // txt_name
+            // 
+            this.txt_name.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_name.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txt_name.Location = new System.Drawing.Point(157, 73);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txt_name.Size = new System.Drawing.Size(308, 34);
+            this.txt_name.TabIndex = 7;
+            this.txt_name.TextChanged += new System.EventHandler(this.txt_name_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(483, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(165, 41);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "اسم الصنف";
+            // 
+            // txt_dat
+            // 
+            this.txt_dat.CalendarMonthBackground = System.Drawing.SystemColors.ScrollBar;
+            this.txt_dat.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txt_dat.Location = new System.Drawing.Point(157, 356);
+            this.txt_dat.Name = "txt_dat";
+            this.txt_dat.Size = new System.Drawing.Size(308, 34);
+            this.txt_dat.TabIndex = 23;
+            // 
+            // FRM_ADD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(718, 660);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FRM_ADD";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cover)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox txt_qun;
+        public System.Windows.Forms.TextBox txt_price;
+        public System.Windows.Forms.TextBox txt_des;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button btn_add;
+        public System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        public System.Windows.Forms.PictureBox pic_cover;
+        public System.Windows.Forms.DateTimePicker txt_dat;
+    }
+}
---
+++ FRM_ADD.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace AMS_PRO_MAX
+{
+    public partial class FRM_ADD : Form
+    {
+        DB_AMS_PROEntities5 db = new DB_AMS_PROEntities5();
+        Item tb_item = new Item();
+        Form1 frm;
+
+
+
+        public int id;
+
+        public FRM_ADD()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            Methods method = new Methods();
+            dialog dialog = new dialog();
+            details details = new details();
+
+            try
+            {
+                var existingItem = db.Items.FirstOrDefault(item => item.Name == txt_name.Text);
+                if (existingItem != null)
+                {
+                    dialog.Width = this.Width;
+                    dialog.label6.Text = "اسم الصنف مكرر";
+                    dialog.Show();
+                }
+                else
+                {
+                    if (txt_name.Text == "")
+                    {
+                        dialog.Width = this.Width;
+                        dialog.label6.Text = "اسم الصنف مطلوب";
+                        dialog.Show();
+                    }
+                    else
+                    {
+
+                        if (id == 0)
+                        {
+                            // Add
+                            tb_item.Name = txt_name.Text;
+                            tb_item.QuantityAvailable = Convert.ToInt32(txt_qun.Text);
+                            tb_item.Price = Convert.ToInt32(txt_price.Text);
+                            tb_item.Description = txt_des.Text;
+                            tb_item.ExpiryDate = txt_dat.Value;
+                            db.Items.Add(tb_item);
+                            db.SaveChanges();
+                            details.txt_det.Text = "تم اضافة صنف";
+                            details.Show();
+                            // تحديث عدد الاشعارات
+                            frm = new Form1();
+                            frm.txt_cot_alert.Text = Convert.ToString(db.Alerts.Count());
+                        }
+                        else
+                        {
+                            // Edit
+                            var item = db.Items.Find(id);
+                            if (item != null)
+                            {
+                                item.Name = txt_name.Text;
+                                item.QuantityAvailable = Convert.ToInt32(txt_qun.Text);
+                                item.Price = Convert.ToInt32(txt_price.Text);
+                                item.Description = txt_des.Text;
+                                item.ExpiryDate = Convert.ToDateTime(txt_dat.Text);
+                                db.SaveChanges();
+                                details.txt_det.Text = "تم تعديل الصنف";
+                                details.Show();
+                            }
+                            else
+                            {
+                                dialog.Width = this.Width;
+                                dialog.label6.Text = "الصنف غير موجود";
+                                dialog.Show();
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void txt_name_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void txt_dat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }   
+    
+}
+
