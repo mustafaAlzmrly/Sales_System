7a64af99aa82839698cc9001b0f2573fee2289ad
mustafa <155826815+mustafaAlzmrly@users.noreply.github.com>
2024-01-20 22:49:21 +0200

    واجهة الاصناف اضافة وحدف وتعديل

---
+++ FRM_HOME.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace AMS_PRO_MAX
+{
+    partial class FRM_HOME
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pn_cat = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.itemIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.expiryDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantityAvailableDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dB_AMS_PRODataSet = new AMS_PRO_MAX.DB_AMS_PRODataSet();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_eidit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_sear = new System.Windows.Forms.TextBox();
+            this.itemsTableAdapter = new AMS_PRO_MAX.DB_AMS_PRODataSetTableAdapters.ItemsTableAdapter();
+            this.pn_cat.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_AMS_PRODataSet)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pn_cat
+            // 
+            this.pn_cat.Controls.Add(this.dataGridView1);
+            this.pn_cat.Controls.Add(this.panel1);
+            this.pn_cat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pn_cat.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pn_cat.Location = new System.Drawing.Point(0, 0);
+            this.pn_cat.Name = "pn_cat";
+            this.pn_cat.Size = new System.Drawing.Size(718, 660);
+            this.pn_cat.TabIndex = 1;
+            this.pn_cat.Paint += new System.Windows.Forms.PaintEventHandler(this.pn_cat_Paint);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Beige;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.itemIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.imageDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.expiryDateDataGridViewTextBoxColumn,
+            this.quantityAvailableDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.itemsBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 26;
+            this.dataGridView1.Size = new System.Drawing.Size(718, 570);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // itemIDDataGridViewTextBoxColumn
+            // 
+            this.itemIDDataGridViewTextBoxColumn.DataPropertyName = "ItemID";
+            this.itemIDDataGridViewTextBoxColumn.HeaderText = "ItemID";
+            this.itemIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.itemIDDataGridViewTextBoxColumn.Name = "itemIDDataGridViewTextBoxColumn";
+            this.itemIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.itemIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.descriptionDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // imageDataGridViewTextBoxColumn
+            // 
+            this.imageDataGridViewTextBoxColumn.DataPropertyName = "Image";
+            this.imageDataGridViewTextBoxColumn.HeaderText = "Image";
+            this.imageDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.imageDataGridViewTextBoxColumn.Name = "imageDataGridViewTextBoxColumn";
+            this.imageDataGridViewTextBoxColumn.ReadOnly = true;
+            this.imageDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            this.priceDataGridViewTextBoxColumn.ReadOnly = true;
+            this.priceDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // expiryDateDataGridViewTextBoxColumn
+            // 
+            this.expiryDateDataGridViewTextBoxColumn.DataPropertyName = "ExpiryDate";
+            this.expiryDateDataGridViewTextBoxColumn.HeaderText = "ExpiryDate";
+            this.expiryDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.expiryDateDataGridViewTextBoxColumn.Name = "expiryDateDataGridViewTextBoxColumn";
+            this.expiryDateDataGridViewTextBoxColumn.ReadOnly = true;
+            this.expiryDateDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // quantityAvailableDataGridViewTextBoxColumn
+            // 
+            this.quantityAvailableDataGridViewTextBoxColumn.DataPropertyName = "QuantityAvailable";
+            this.quantityAvailableDataGridViewTextBoxColumn.HeaderText = "QuantityAvailable";
+            this.quantityAvailableDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.quantityAvailableDataGridViewTextBoxColumn.Name = "quantityAvailableDataGridViewTextBoxColumn";
+            this.quantityAvailableDataGridViewTextBoxColumn.ReadOnly = true;
+            this.quantityAvailableDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // itemsBindingSource
+            // 
+            this.itemsBindingSource.DataMember = "Items";
+            this.itemsBindingSource.DataSource = this.dB_AMS_PRODataSet;
+            this.itemsBindingSource.CurrentChanged += new System.EventHandler(this.itemsBindingSource_CurrentChanged);
+            // 
+            // dB_AMS_PRODataSet
+            // 
+            this.dB_AMS_PRODataSet.DataSetName = "DB_AMS_PRODataSet";
+            this.dB_AMS_PRODataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.btn_delete);
+            this.panel1.Controls.Add(this.btn_eidit);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txt_sear);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 570);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(718, 90);
+            this.panel1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::AMS_PRO_MAX.Properties.Resources.find_32x32;
+            this.button5.Location = new System.Drawing.Point(659, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 64);
+            this.button5.TabIndex = 7;
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::AMS_PRO_MAX.Properties.Resources.convert_32x32;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(338, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 64);
+            this.button4.TabIndex = 6;
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Image = global::AMS_PRO_MAX.Properties.Resources.cancel_32x32;
+            this.btn_delete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_delete.Location = new System.Drawing.Point(222, 12);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(99, 64);
+            this.btn_delete.TabIndex = 5;
+            this.btn_delete.Text = "حذف";
+            this.btn_delete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_eidit
+            // 
+            this.btn_eidit.Image = global::AMS_PRO_MAX.Properties.Resources.editcontact_32x32;
+            this.btn_eidit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_eidit.Location = new System.Drawing.Point(117, 12);
+            this.btn_eidit.Name = "btn_eidit";
+            this.btn_eidit.Size = new System.Drawing.Size(99, 64);
+            this.btn_eidit.TabIndex = 4;
+            this.btn_eidit.Text = "تعديل";
+            this.btn_eidit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_eidit.UseVisualStyleBackColor = true;
+            this.btn_eidit.Click += new System.EventHandler(this.btn_eidit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::AMS_PRO_MAX.Properties.Resources.addfile_32x32;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(11, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "اضافة";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_sear
+            // 
+            this.txt_sear.Font = new System.Drawing.Font("Segoe UI", 26F);
+            this.txt_sear.Location = new System.Drawing.Point(431, 12);
+            this.txt_sear.Name = "txt_sear";
+            this.txt_sear.Size = new System.Drawing.Size(222, 65);
+            this.txt_sear.TabIndex = 4;
+            // 
+            // itemsTableAdapter
+            // 
+            this.itemsTableAdapter.ClearBeforeFill = true;
+            // 
+            // FRM_HOME
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 660);
+            this.Controls.Add(this.pn_cat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FRM_HOME";
+            this.Text = "FRM_HOME";
+            this.Load += new System.EventHandler(this.FRM_HOME_Load);
+            this.pn_cat.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_AMS_PRODataSet)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel pn_cat;
+        public System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Button button5;
+        public System.Windows.Forms.Button button4;
+        public System.Windows.Forms.Button btn_delete;
+        public System.Windows.Forms.Button btn_eidit;
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox txt_sear;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itemIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn imageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn expiryDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantityAvailableDataGridViewTextBoxColumn;
+        public DB_AMS_PRODataSet dB_AMS_PRODataSet;
+        public System.Windows.Forms.BindingSource itemsBindingSource;
+        public DB_AMS_PRODataSetTableAdapters.ItemsTableAdapter itemsTableAdapter;
+    }
+}
---
+++ FRM_HOME.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace AMS_PRO_MAX
+{
+    public partial class FRM_HOME : Form
+    {
+        Form1 frm;
+        //FRM_ADD eidt;
+        DB_AMS_PROEntities5 db;
+        Item add;
+        dialog dialo;
+        User User;
+        public int id;
+        public FRM_HOME()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FRM_ADD fadd = new FRM_ADD();
+            fadd.Show();
+        }
+
+        private void pn_cat_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FRM_HOME_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dB_AMS_PRODataSet.Items' table. You can move, or remove it, as needed.
+            this.itemsTableAdapter.Fill(this.dB_AMS_PRODataSet.Items);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void itemsBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            db = new DB_AMS_PROEntities5();
+            dataGridView1.DataSource = db.Items.ToList();
+            frm = new Form1();
+            frm.txt_cot_alert.Text = Convert.ToString(db.Alerts.Count());
+
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            Delete_Item();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Delete_Item()
+        {
+            try
+            {
+                db = new DB_AMS_PROEntities5();
+                User = new User();
+
+                add = new Item();
+                dialo = new dialog();
+
+                if (txt_sear.Text != "")
+                {
+                    id = Convert.ToInt32(txt_sear.Text); // تحديد الصف المحدد واستخراج قيمة العمود "ItemID
+
+                    if (id != 0)
+                    {
+                        var dialogResult = MessageBox.Show("هل أنت متأكد من هذا الإجراء؟ لا يمكن استرجاع البيانات، سيتم حذف جميع البيانات المرتبطة", "إجراء الحذف", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            add = db.Items.FirstOrDefault(x => x.ItemID == id);
+
+                            if (add != null)
+                            {
+                                frm = new Form1();
+                                db.Items.Remove(add); // حذف العنصر من قاعدة البيانات
+                                db.SaveChanges();
+
+                                dialo.label6.Text = "تم حذف الصنف بواسطة ";
+                                dialo.Show();
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("لا يوجد بيانات لحذفها", "لا يمكن إجراء العملية", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("لا يوجد بيانات لحذفها", "لا يمكن إجراء العملية", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                }
+                else
+                {
+                    dialo.label6.Text = "ادخل id لحذف";
+                }
+                
+
+               
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("حدث خطأ في الاتصال بقاعدة البيانات: " + ex.Message, "خطأ في الاتصال", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_eidit_Click(object sender, EventArgs e)
+        {
+            db = new DB_AMS_PROEntities5();
+            if (txt_sear.Text != "") 
+            {                 
+                int searchId = Convert.ToInt32(txt_sear.Text);
+            var item = db.Items.Find(searchId);
+
+            if (item != null)
+            {
+                FRM_ADD editForm = new FRM_ADD();
+                editForm.id = searchId;
+                editForm.txt_name.Text = item.Name;
+                    editForm.btn_add.Text = "تعديل";
+                editForm.Show();
+            }
+            else
+            {
+                dialo = new dialog();
+                dialo.label6.Text = "لم يتم العتور على الصنف الدي تريد تعديله";
+                dialo.Show();
+
+
+            }}
+            else
+            {
+                dialo = new dialog();
+                dialo.label6.Text = "لتعديل الصنف id ادخل ";
+                dialo.Show();
+            }
+            
+
+        }
+    }
+}
