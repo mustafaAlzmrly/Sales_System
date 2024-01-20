3d07e16f6defd1f3401bdb85a9622f7e4098ce3f
mustafa <155826815+mustafaAlzmrly@users.noreply.github.com>
2024-01-20 22:38:31 +0200

    Add files via upload

---
+++ Login.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace AMS_PRO_MAX
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.edt_username = new System.Windows.Forms.TextBox();
+            this.edt_password = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(60, 542);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(404, 71);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "دخول";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.BackgroundImage = global::AMS_PRO_MAX.Properties.Resources.pngtree_futuristic_user_interface_hud_sci_fi_virtual_reality_technology_view_display_picture_image_1177858;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.edt_username);
+            this.panel1.Controls.Add(this.edt_password);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(518, 645);
+            this.panel1.TabIndex = 25;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::AMS_PRO_MAX.Properties.Resources.main_thumb_pb_1031889_200_lvdbswckoufagfajveqbjflhmfuazojc;
+            this.pictureBox1.Location = new System.Drawing.Point(168, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(177, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 39);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "اسم المستخدم";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(207, 412);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 39);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "كلمة السر";
+            // 
+            // edt_username
+            // 
+            this.edt_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edt_username.Location = new System.Drawing.Point(23, 345);
+            this.edt_username.Name = "edt_username";
+            this.edt_username.Size = new System.Drawing.Size(474, 45);
+            this.edt_username.TabIndex = 2;
+            this.edt_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // edt_password
+            // 
+            this.edt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edt_password.Location = new System.Drawing.Point(23, 459);
+            this.edt_password.Name = "edt_password";
+            this.edt_password.PasswordChar = '*';
+            this.edt_password.Size = new System.Drawing.Size(474, 45);
+            this.edt_password.TabIndex = 2;
+            this.edt_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(518, 645);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(536, 692);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Login_FormClosed);
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.Leave += new System.EventHandler(this.Login_Leave);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox edt_username;
+        public System.Windows.Forms.TextBox edt_password;
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AMS_PRO_MAX
+{
+    public partial class Login : Form
+    {
+        DB_AMS_PROEntities4 db = new DB_AMS_PROEntities4();
+        Form1 main = new Form1();
+        User use = new User();
+        
+        public int id;
+        bool state;
+        User add;
+        
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void txt_user_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_pass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void show_btn()
+        {
+            Form1 frm = new Form1();
+            frm.button1.Enabled = true;
+
+        }
+        private bool Save()
+        {
+            dialog dialog = new dialog();
+            // check fields
+
+            if (edt_username.Text == "" || edt_password.Text == "")
+            {
+                dialog.Width = this.Width;
+                dialog.label6.Text = "الحقل مطلوب";
+                dialog.Show();
+            }
+        
+            else
+            {
+                // Add or edit
+                if (id == 0)
+                {
+                    // Add
+                    // Check Duplicate Data
+
+
+
+                    // Add
+                    AddData();
+                    state = true;
+
+
+                }
+
+            }
+            return state;
+       }
+        private void AddData()
+        {
+
+            try
+            {
+                details details = new details();
+                dialog dialog = new dialog();
+                db = new DB_AMS_PROEntities4();
+                add = new User();
+                var username = edt_username.Text;
+                var password = edt_password.Text;
+                add = db.Users.Where(x => x.Username == username && x.Password == password).FirstOrDefault();
+                if (add != null)
+                {
+                    // Login
+
+                    main = new Form1();
+                    main.txt_username.Text = add.Fullname;
+                   // add.UserState = 1;
+                    db.Entry(add).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+
+
+
+    
+
+                    main.Show();
+
+                    this.Hide();
+
+
+                    //كنت مداير form1 هي الواجهة الرئسية
+
+                   /* main.Enabled = true;
+                    main.button1.Enabled = true;
+                    main.button2.Enabled = true;
+                    main.button3.Enabled = true;
+                    main.button9.Enabled = true;
+                    main.button10.Enabled = true;
+                    main.btn_alert.Enabled = true;
+                    main.btn_login.Enabled = false;
+                    main.btn_login.Visible = false;
+                    main.btn_cancel.Visible = true;
+                    main.txt_cot_alert.Visible = true;*/
+                    //
+                    
+                
+
+                }
+                else
+                {
+                    dialog.Width = this.Width;
+                    dialog.label6.Text = "خطأ في معلومات تسجيل الدخول";
+                    dialog.Show();
+                }
+
+
+
+            }
+            catch
+            {
+                
+
+            }
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Save();
+
+
+        }
+
+        private void Login_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //main.Show();
+        }
+
+        private void Login_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Login_Leave(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
